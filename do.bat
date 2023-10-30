73451cd4e4406f601bfa80c0888689d7210ae7c7
linan1109 <linan119@yeah.net>
2023-10-30 08:48:36 +0000

    use dev container

---
+++ do.bat
@@ -0,0 +1,11 @@
+
+sacrebleu data/en-sv/bible_uedin/raw/test.en -i assignments/01/baseline/big/bible_translations.p.txt -m bleu chrf --chrf-word-order  1
+sacrebleu data/en-sv/bible_uedin/raw/test.en -i assignments/01/baseline/big/bible_translations.p.txt -m bleu chrf --chrf-word-order  2
+sacrebleu data/en-sv/bible_uedin/raw/test.en -i assignments/01/baseline/big/bible_translations.p.txt -m bleu chrf --chrf-word-order  3
+sacrebleu data/en-sv/bible_uedin/raw/test.en -i assignments/01/baseline/big/bible_translations.p.txt -m bleu chrf --chrf-word-order  4
+sacrebleu data/en-sv/bible_uedin/raw/test.en -i assignments/01/baseline/big/bible_translations.p.txt -m bleu chrf --chrf-word-order 5
+sacrebleu data/en-sv/bible_uedin/raw/test.en -i assignments/01/baseline/big/bible_translations.p.txt -m bleu chrf --chrf-word-order 6
+sacrebleu data/en-sv/bible_uedin/raw/test.en -i assignments/01/baseline/big/bible_translations.p.txt -m bleu chrf --chrf-word-order 7
+sacrebleu data/en-sv/bible_uedin/raw/test.en -i assignments/01/baseline/big/bible_translations.p.txt -m bleu chrf --chrf-word-order 8
+sacrebleu data/en-sv/bible_uedin/raw/test.en -i assignments/01/baseline/big/bible_translations.p.txt -m bleu chrf --chrf-word-order 9
+sacrebleu data/en-sv/bible_uedin/raw/test.en -i assignments/01/baseline/big/bible_translations.p.txt -m bleu chrf --chrf-word-order 10
