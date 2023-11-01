0a6cb18b317cb4b0253d04e0dfbb8a35c630ea00
linan1109 <linan119@yeah.net>
2023-11-01 16:51:02 +0100

    tune hyperparameter learning rate

--- assignments/03/tune/tune--lr.sh
+++ assignments/03/tune/tune--lr.sh
@@ -22,28 +22,11 @@ for lr in 0.0001 0.0005 0.001 0.005 0.01
 do 
     mkdir -p $data/$tune/lr_$lr
     python train.py \
-        --cuda \
         --data $data/prepared/ \
         --source-lang $src \
         --target-lang $tgt \
         --lr $lr \
-        --batch-size 1 \
-        --max-epoch 10000 \
-        --patience 3 \
-        --save-dir $data/$tune/lr_$lr \
-         --restore-file checkpoint-last.pt \
-        --save-interval 1 \
-        --encoder-embed-dim 64 \
-        --encoder-hidden-size 64 \
-        --encoder-num-layers 1 \
-        --encoder-bidirectional True \
-        --encoder-dropout-in 0.25 \
-        --encoder-dropout-out 0.25 \
-        --decoder-embed-dim 64 \
-        --decoder-hidden-size 128 \
-        --decoder-num-layers 1 \
-        --decoder-dropout-in 0.25 \
-        --decoder-dropout-out 0.25
+        --save-dir $data/$tune/lr_$lr
 done
 
 echo "train done!"
@@ -53,7 +36,6 @@ echo "train done!"
 for lr in 0.0001 0.0005 0.001 0.005 0.01
 do
     python translate.py \
-        --cuda \
         --data $data/prepared/ \
         --dicts $data/prepared/ \
         --checkpoint-path $data/$tune/lr_$lr/checkpoint_last.pt \
