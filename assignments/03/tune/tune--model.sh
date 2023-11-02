8a7be4756f2558d838fb772d528b471c355701d8
linan1109 <linan119@yeah.net>
2023-11-02 06:59:18 +0000

    tune hyperparameter model num layers

--- assignments/03/tune/tune--model.sh
+++ assignments/03/tune/tune--model.sh
@@ -21,35 +21,18 @@ for num_layers in 1 2 3
 do 
     mkdir -p $data/$tune/num_layers_$num_layers
     python train.py \
-        --cuda \
         --data $data/prepared/ \
         --source-lang $src \
         --target-lang $tgt \
-        --lr 0.003 \
-        --batch-size 1 \
-        --max-epoch 10000 \
-        --patience 3 \
         --save-dir $data/$tune/num_layers_$num_layers \
-         --restore-file checkpoint-last.pt \
-        --save-interval 1 \
-        --encoder-embed-dim 64 \
-        --encoder-hidden-size 64 \
         --encoder-num-layers $num_layers \
-        --encoder-bidirectional True \
-        --encoder-dropout-in 0.25 \
-        --encoder-dropout-out 0.25 \
-        --decoder-embed-dim 64 \
-        --decoder-hidden-size 128 \
-        --decoder-num-layers $num_layers \
-        --decoder-dropout-in 0.25 \
-        --decoder-dropout-out 0.25
+        --decoder-num-layers $num_layers 
 done
 
 echo "train done!"
 for num_layers in 1 2 3
 do
     python translate.py \
-        --cuda \
         --data $data/prepared/ \
         --dicts $data/prepared/ \
         --checkpoint-path $data/$tune/num_layers_$num_layers/checkpoint_last.pt \
